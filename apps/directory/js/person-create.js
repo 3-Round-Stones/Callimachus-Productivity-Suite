a69590fb9c8285bac44bfb59be38f08dde8b43ea
Luke Ruth <luke.ruth@gmail.com>
2013-07-24 16:33:31 -0400

    Improved Directory App
    Adding functionality to the directory app for creation, viewing, and
    editing. Still trying to figure out why certain fields are being
    duplicated on creation.

--- apps/directory/js/person-create.js
+++ apps/directory/js/person-create.js
@@ -2,6 +2,32 @@ $(document).ready(function() {
     $(".initHide").hide();
 });
 
+function submitHandler(event) {
+    $(".controls").each(function( groupIndex ) {
+        var group = $(this);
+        var empty = true;
+        
+        group.find('input').each(function( inputIndex ) {
+            if ($(this).attr('type') == "text" || $(this).attr('type') == "date" || $(this).attr('type') == "number" 
+                || $(this).attr('type') == "tel" || $(this).attr('type') == "email" || $(this).attr('type') == "url") {
+                if ($(this).val()) {
+                    alert('Group: ' + groupIndex + '. Input: ' + inputIndex + '. Has value: ' + $(this).val() + ' and ID: ' + $(this).attr('id'));
+                    empty = false;
+                }
+            } else if ($(this).attr('type') == "radio") {
+                if ($(this).checked) {
+                    empty = false;
+                }
+            }
+        });
+        if (empty == true) {
+            alert('Emptying group: ' + groupIndex);
+            group.empty();
+        }
+    });
+    return calli.saveResourceAs(event,calli.slugify($('#givenName').val() + ' ' + $('#familyName').val()))
+}
+
 function toggleInput(link) {
     var group = $(link).closest('.control-group');
     group.find('.controls').toggle('slow');
